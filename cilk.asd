cdd39eaad7575c630a790b15e28e511ada4fd3db
Max Mikhanosha <max@momoland.openchat.com>
2009-07-06 15:20:37 -0400

    Added mfence instruction

--- cilk.asd
+++ cilk.asd
@@ -7,6 +7,7 @@
   :components (
                (:file "cilk-package")
                (:file "catch-case")
+               (:file "memory-barrier")
                (:file "cilk-runner")
                (:file "cilk")))
 
---
+++ memory-barrier.lisp
@@ -0,0 +1,12 @@
+(cl:in-package :cilk)
+
+(sb-c::defknown mfence () (values))
+;; X86-64 memory fence instruction
+(sb-vm::define-vop (mfence)
+          (:policy :fast-safe)
+           (:translate mfence)
+           (:generator 1
+                       (sb-vm::inst sb-vm::byte #x0f)
+                       (sb-vm::inst sb-vm::byte #xae)
+                       (sb-vm::inst sb-vm::byte #xf0)))
+
