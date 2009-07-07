b7eaae18c248ec8fc89b946a0219f45a503d3b64
Max Mikhanosha <max@momoland.openchat.com>
2009-07-07 18:45:34 -0400

    indicate that (mfence) has no side effects

--- memory-barrier.lisp
+++ memory-barrier.lisp
@@ -3,10 +3,12 @@
 (sb-c::defknown mfence () (values))
 ;; X86-64 memory fence instruction
 (sb-vm::define-vop (mfence)
-          (:policy :fast-safe)
-           (:translate mfence)
-           (:generator 1
-                       (sb-vm::inst sb-vm::byte #x0f)
-                       (sb-vm::inst sb-vm::byte #xae)
-                       (sb-vm::inst sb-vm::byte #xf0)))
+  (:policy :fast-safe)
+  (:translate mfence)
+  (:effects)
+  (:affected)
+  (:generator 0
+              (sb-vm::inst sb-vm::byte #x0f)
+              (sb-vm::inst sb-vm::byte #xae)
+              (sb-vm::inst sb-vm::byte #xf0)))
 
